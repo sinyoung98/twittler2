0c24de33f6a411b7b3d5b83700d993ac9c4bec4a
sinyoung98 <45649186+sinyoung98@users.noreply.github.com>
2020-07-03 10:30:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 
 // DATA는 이미 작성된 트윗을 표시합니다.
 console.log(DATA)
-
 // generateNewTweet을 호출할 때마다 새로운 트윗을 생성합니다.
 console.log(generateNewTweet());
-
+creatDatatweets();
+document.querySelector('#btn-tweet').onclick = creatNewTweet;
 document.getElementById('test').innerHTML = 'hello twittler, check developer console!';
